ba6cd73ae40d0bce7b80c616bc04c642f5df40df
miguel-willians <miiguel.wso@gmail.com>
2025-05-25 13:15:47 -0300

    fix: concertar interação do quadrado com os tilemaps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,10 +6,12 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 7f;
     private Rigidbody2D rb;
     private bool isGrounded = false;
+    private Vector3 startPosition;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        startPosition = transform.position;
     }
 
     void Update()
@@ -26,16 +27,17 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Verifica se está no chão
+        Debug.Log("Colidiu com: " + collision.gameObject.tag);
+
         if (collision.contacts[0].normal.y > 0.5f)
             isGrounded = true;
 
-        // Verifica se a plataforma corresponde à cor atual
-        if (collision.gameObject.CompareTag("SilverPlatform") && ColorManager.Instance.currentColor != ColorManager.PlayerColor.Silver)
-            ReloadScene();
+        CheckColorCollision(collision.gameObject);
+    }
 
-        if (collision.gameObject.CompareTag("GoldPlatform") && ColorManager.Instance.currentColor != ColorManager.PlayerColor.Gold)
-            ReloadScene();
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        CheckColorCollision(collision.gameObject);
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -43,8 +45,25 @@ public class PlayerController : MonoBehaviour
         isGrounded = false;
     }
 
-    void ReloadScene()
+    void CheckColorCollision(GameObject obj)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (obj.CompareTag("GoldTilemap") && ColorManager.Instance.currentColor != ColorManager.PlayerColor.Gold)
+        {
+            Debug.Log("Cor incorreta no GoldTilemap! Respawn.");
+            Respawn();
+        }
+
+        if (obj.CompareTag("SilverTilemap") && ColorManager.Instance.currentColor != ColorManager.PlayerColor.Silver)
+        {
+            Debug.Log("Cor incorreta no SilverTilemap! Respawn.");
+            Respawn();
+        }
+    }
+
+    void Respawn()
+    {
+        transform.position = startPosition;
+        rb.linearVelocity = Vector2.zero;
+        Debug.Log("Respawn realizado.");
     }
 }
